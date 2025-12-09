3e5ca0d43e506c852ef43d2da6d53cad14b9bb0b
tiago-sb <belatiago72@gmail.com>
2025-12-09 05:12:14 -0300

    modifica estrutura do analisador léxico

--- analisadorSintaticoSLR/Main.java
+++
@@ -1,7 +0,0 @@
-import view.AnalisadorSintaticoView;
-
-public class Main {
-    public static void main(String[] args) {
-        AnalisadorSintaticoView.launch(AnalisadorSintaticoView.class, args);
-    }
-}
